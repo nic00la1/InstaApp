b19811a2931d811c4ed46de8750aeac84655e9aa
Nicola Kaleta <nic00la.kaleta@gmail.com>
2025-11-14 09:18:05 +0100

    Dodaj pliki projektów.

---
+++ InstaApp/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "InstaApp")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "InstaApp.Pages")]
